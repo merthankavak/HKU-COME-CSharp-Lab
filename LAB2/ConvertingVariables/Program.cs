f2da84916125a6ac3362d7e65e328b65ea9691b2
Merthan Kavak <48051722+merthankavak@users.noreply.github.com>
2019-11-09 16:52:56 +0300

    Add files via upload

---
+++ LAB2/ConvertingVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace convertingvariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intString = "456", intString2 = "457";
+            int int_var = Convert.ToInt32(intString);
+            int int_var2 = int.Parse(intString2);
+            float float_var = 10.155F;
+            double double_var = 854.32;
+            Console.WriteLine("Converting string to int with 'Convert.ToInt32()' Method: " + int_var);
+            Console.WriteLine("Converting string to int with 'int.Parse()' Method: " + int_var2);
+            Console.WriteLine("Converting float to int with 'Convert.ToInt32()' Method: " + Convert.ToInt32(float_var));
+            Console.WriteLine("Converting double to int with 'Convert.ToInt32()' Method: " + Convert.ToInt32(double_var));
+
+
+        }
+    }
+}
---
+++ LAB2/PerfectNumber/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            int number;
+            if (!int.TryParse(str, out number))
+            {
+                MessageBox.Show("Please Enter a Number");
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 1; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum==number)
+                    MessageBox.Show("Entered number is a perfect number");
+                else
+                    MessageBox.Show("Entered number is NOT a perfect number");
+
+            }
+        }
+    }
+}
